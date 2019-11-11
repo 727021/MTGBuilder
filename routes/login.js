337e2c29e4d5c870ee8256a4c22242b84195ec57
727021 <1834544+727021@users.noreply.github.com>
2019-11-11 10:50:18 -0700

    Test postgres connection

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,15 @@
 var express = require('express');
+const {Client} = require('pg')
 var router = express.Router();
+
+const pgClient = new Client(process.env.DATABASE_URL || {
+    user: 'aschim',
+    host: 'localhost',
+    database: 'mtgbuilder_dev',
+    password: 'aschim_dev_mtg',
+    port: 5432
+})
+pgClient.connect((err) => { if (err) console.error(err) })
 
 /* GET home page. */
 router.get('/login', function(req, res, next) {
@@ -7,6 +17,10 @@ router.get('/login', function(req, res, next) {
         // Do login
         res.send({login:'fail',id:0})
     } else {
+        pgClient.query("SELECT 'Hello World' AS result", (err, res) => {
+            if (!err) console.log(res.rows[0].result)
+            pgClient.end()
+        })
         res.render('profile', { title: "Login" });
     }
 });
