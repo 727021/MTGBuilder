1fc771ebf585facc17226a3de6a9a3b067f34d04
727021 <1834544+727021@users.noreply.github.com>
2019-11-14 15:17:46 -0700

    Fix view name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'MTGBuilder' });
+  res.render('index', { title: 'MTGBuilder' });
 });
 
 module.exports = router;
