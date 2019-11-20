e255a1db66ff9664e5d9fc80170721cf2af11137
727021 <1834544+727021@users.noreply.github.com>
2019-11-19 19:17:14 -0700

    Fix CSS

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,15 @@
 $(() => {
     $('#btnLogin').click(() => {
-        $('#invalidLogin').fadeOut()
+        $('#invalidLogin').slideUp()
         let user = $('#username').val().trim()
         let pass = $('#password').val().trim()
 
         if (user == '' || pass == '') {
-            return $('#invalidLogin').fadeIn()
+            return $('#invalidLogin').slideDown()
         }
         $.get('/ajax/login', {user: user, password: pass}, (data) => {
             console.log(data)
-            if (data.error) return $('#invalidLogin').fadeIn()
+            if (data.error) return $('#invalidLogin').slideDown()
             document.location.replace('/')
         })
     })
