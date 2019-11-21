f8fbe2e9c58701f38765eb52f4642b8d3d27bda3
727021 <1834544+727021@users.noreply.github.com>
2019-11-21 15:11:56 -0700

    Add card list modal

--- public/js/searchCard.js
+++ public/js/searchCard.js
@@ -22,7 +22,7 @@ $(() => {
                     <tr>
                         <td class="align-middle"><span class="text-muted">${++cardCount}</span></td>
                         <td class="align-middle"><i data-toggle="tooltip" data-placement="top" title="${card.setName}" class="ss ss-grad ss-2x ss-${card.set.toLowerCase()} ss-${card.rarity.toLowerCase()}"></i></td>
-                        <td class="align-middle"><span class="searchName">${card.name}</span>${(names) ? '/' + names.join('/') + '<sup><a href="#note" class="text-reset text-decoration-none">&Dagger;</a></sup>' : ''}</td>
+                        <td class="align-middle"><span data-img="${card.imageUrl || ''}" class="searchName">${card.name}</span>${(names) ? '/' + names.join('/') + '<sup><a href="#note" class="text-reset text-decoration-none">&Dagger;</a></sup>' : ''}</td>
                         <td class="align-middle">${card.types.join(', ')}</td>
                         <td class="align-middle">${(card.manaCost || '').toLowerCase().replace(/{/g, '<i class="ml-1 ms ms-cost ms-').replace(/}/g, '"></i>')}</td>
                         <td class="align-middle">${(card.power && card.toughness) ? `${card.power}/${card.toughness}` : card.loyalty ? `${card.loyalty}<sup><sup><a href="#note" class="text-reset text-decoration-none">&dagger;</a></sup></sup>` : ''}</td>
@@ -38,6 +38,18 @@ $(() => {
                     $('#btnLoadMore').attr('disabled', '')
                 }
                 $('[data-toggle="tooltip"]').tooltip()
+                $('.searchName').unbind()
+                $('.searchName').each(function() {
+                    console.log($(this))
+                    $(this).click(function() {
+                        let img = $(this).attr('data-img')
+                        let name = $(this).text()
+
+                        $('#cardModalTitle').html(name)
+                        $('#cardModalContent').html(`<img src="${img}" class="card img-fluid mx-auto">`)
+                        $('#cardModal').modal('show')
+                    })
+                })
             }
         })
     }
