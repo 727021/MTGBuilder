ac32ce6e64acdae6b6a4e59e8d9fbe8dab3d8e91
727021 <1834544+727021@users.noreply.github.com>
2019-11-26 09:10:17 -0700

    Add special cases for some planeswalker loyalties

--- public/js/common.js
+++ public/js/common.js
@@ -69,7 +69,7 @@ function parseSymbols(text) {
         .replace(/["\u0022\u201C\u201D]/g, '&quot;')// " to &quot;
         .replace(/\\/g, '')// Remove backslashes
         .replace(/{CHAOS}/g, '<i class="ms ms-chaos"></i>')// Chaos symbol
-        .replace(/{(\d)}/g, '<i class="ms ms-cost ms-shadow ms-$1"></i>')// Colorless numbered mana
+        .replace(/{(\d+)}/g, '<i class="ms ms-cost ms-shadow ms-$1"></i>')// Colorless numbered mana
         .replace(/{[\dhwbrgucpsxyz]+(?:\/[wbrgucps])?}/gi,(match) => {return match.toLowerCase()})
         .replace(/{([wbrgucpsxyz])}/g, '<i class="ms ms-cost ms-shadow ms-$1"></i>')// Normal mana
         .replace(/{h([wbrgucpsxyz])}/g, '<i class="ms ms-cost ms-half ms-shadow ms-$1"></i>')// Half mana
@@ -77,8 +77,10 @@ function parseSymbols(text) {
         .replace(/[\r\n]/g, '<br>')// \n and \r to <br>
         .replace(/{T}/g, '<i class="ms ms-cost ms-shadow ms-tap"></i>')// Tap symbol
         .replace(/{Q}/g, '<i class="ms ms-cost ms-shadow ms-untap"></i>')// Untap symbol
+        .replace(/{\u221E}/g, '<i class="ms ms-cost ms-shadow ms-infinity"></i>')// Infinity mana
+        .replace(/{\u00BD}/g, '<i class="ms ms-cost ms-shadow ms-1-2"></i>')// 1/2 mana
         .replace(/\[0\]/g, '<i class="ms ms-loyalty-zero ms-loyalty-0"></i>')// Zero loyalty
         .replace(/\[\+(\d+)\]/g, '<i class="ms ms-loyalty-up ms-loyalty-$1"></i>')// Up loyalty
-        .replace(/\[−(\d+)\]/g, '<i class="ms ms-loyalty-down ms-loyalty-$1"></i>')// Down loyalty
+        .replace(/\[[-−](\d+)\]/g, '<i class="ms ms-loyalty-down ms-loyalty-$1"></i>')// Down loyalty
         .replace(/(\(.+\))/g, '<i>$1</i>')// Italicize help text
 }
